c8b07c5eef041462bd597450bff06423bb0bd800
Karim Khalifa <karimkhalifa@hotmail.com>
2019-09-28 16:47:43 +1000

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Row, Col, Container, Jumbotron, Button, FormControl, ButtonToolbar} from 'react-bootstrap'
 import './App.css';
+
+const startGame = () =>{
+  alert("Game started!");
+  let pTable = document.getElementById("playerTable");
+  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container id="Container">
+          <Row>
+            <Col>
+              <Jumbotron className="bjTable">
+                <h2>Player</h2>
+                <div className="table" id="playerTable"></div>
+              </Jumbotron>
+            </Col>
+            <Col>
+              <Jumbotron className="bjTable">
+                <h2>Dealer</h2>
+                <div className="table" id="dealerTable"></div>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Jumbotron>
+          <Row>
+            <Col>
+              <p>Available Balance: <span id="availableBalance">1000</span></p>
+            </Col>
+            <Col>
+              <FormControl placeholder="Enter the amount you're willing to bet!"></FormControl>
+            </Col>
+            <Col>
+            <ButtonToolbar>
+              <Button onClick={startGame} style={{"marginRight": "10px"}}>Bet</Button>
+              <Button style={{"marginRight": "10px"}} variant="success">Hit me!</Button>
+              <Button variant="info">Stand</Button>
+            </ButtonToolbar>
+            </Col>
+          </Row>
+          </Jumbotron>
+      </Container>
     </div>
   );
 }
