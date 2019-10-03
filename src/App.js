85bea9b711b39930116b513134913c9bc89be541
Karim Khalifa <karimkhalifa@hotmail.com>
2019-10-03 15:54:59 +1000

    OOP just needs some refactoring, still buggy

--- src/App.js
+++ src/App.js
@@ -1,224 +1,29 @@
 import React , {Component} from 'react';
 import {Row, Col, Container, Jumbotron, Button, FormControl, ButtonToolbar} from 'react-bootstrap'
 import './App.css';
+import { Game, Cards} from './classes'
 
-let cardColor, highCards, dScore, pScore, dRes, betArea, avBalance;
+let dScore, pScore, dRes, betArea, avBalance;
 let pTable, dTable, hButton, sButton, nButton, bButton, stButton;
-let dResManip, pCards, dCards;
+let pCards, dCards, blackJack;
 
-class ElementManipulation{
 
-  constructor(el){
-    this.elem = el;
-  }
-    delElem = () => {
-    while (this.elem.firstChild) {
-      this.elem.removeChild(this.elem.firstChild);
-    }
-  }
-  appElem = (txt) => {
-    let appendElem = document.createElement("p");
-    appendElem.innerText = txt;
-    this.elem.appendChild(appendElem);
-  }
-  repElem = (txt) => {
-    let newElem = document.createElement("p");
-    newElem.innerText = txt;
-    this.elem.replaceChild(newElem,this.elem.firstChild);
+
+
+function checkInt(event){
+  let value = parseInt(event.target.value)
+  console.log(value)
+  if( isNaN(value)){
+    alert("Please enter a valid NUMBER!");
+    this.betArea.value = ""
   }
 }
 
-class Game{
 
-  updateBalance = (num) => {
-    console.log(avBalance.innerHTML)
-    if( num === 1)
-      avBalance.innerText = parseInt(avBalance.innerText) + parseInt(betArea.value)*2;
-    else if ( num === 2)
-      avBalance.innerText = parseInt(avBalance.innerText) + Math.ceil(parseInt(betArea.value)*(5/2));
-    else if ( num === 3)
-      avBalance.innerText = parseInt(avBalance.innerText) + parseInt(betArea.value);
-  }
-  
-  checkInt(event){
-    let value = parseInt(event.target.value)
-    console.log(value)
-    if( isNaN(value)){
-      alert("Please enter a valid NUMBER!");
-      betArea.value = ""
-    }
-  }
 
-  dealerPlay = () => {
-    let player = parseInt(pScore.firstChild.innerText)
-    hButton.style.display = "none";
-    sButton.style.display = "none";
-    dTable.removeChild(dTable.firstChild);
-    while (parseInt(dScore.firstChild.innerText) < 15)
-       dCards.drawCard();
-    if(avBalance.innerHTML == 0 && (parseInt(dScore.firstChild.innerText) <= 21 && parseInt(dScore.firstChild.innerText) > player)){
-      dResManip.appElem("The House Wins! Play Again?");
-      dRes.style.color = "red";
-      stButton.style.display = "block";
-      nButton.style.display = "none";
-    }else if(parseInt(dScore.firstChild.innerText) <= 21 && parseInt(dScore.firstChild.innerText) > player ){
-      dResManip.appElem("The dealer wins this round!");
-      dRes.style.color = "red";
-      nButton.style.display = "block";
-    }else if (parseInt(dScore.firstChild.innerText)=== player){
-      dResManip.appElem("Draw!")
-      dRes.style.color = "blue";
-      this.updateBalance(3);
-      nButton.style.display = "block";
-    }else{
-      dResManip.appElem("Congrats, you win this round!");
-      dRes.style.color = "green";
-      this.updateBalance(1);
-      nButton.style.display = "block";
-    }
-  }
-  
-  nextRound = () => {
-    pCards.clearCardsTable();
-    dCards.clearCardsTable();
-    nButton.style.display = "none";
-  }
-  
-  newGame = () => {
-    this.nextRound();
-    avBalance.innerHTML = 1000;
-    stButton.style.display = "none";
-  }
-  
-  startGame = () => {
-    let betValue = betArea.value;
-    console.log(betValue.length)
-  
-    if( !betValue.length ){
-      alert("Don't forget to enter a bet!")
-    }else if (betValue <= parseInt(avBalance.innerHTML)){
-      betArea.setAttribute("disabled","");
-      avBalance.innerHTML = parseInt(avBalance.innerHTML) - parseInt(betValue);
-      pCards.dispStartCards();
-      dCards.dispStartCards();
-      bButton.style.display = "none";
-      hButton.style.display = "block";
-      sButton.style.display = "block";
-    }else if(avBalance.innerHTML === 0){
-      stButton.style.display = "block";
-    }else{
-      alert("Cannot bet more than you have!")
-    }
-  }
-  
-  
-  checkScore = () => {
-    let score = parseInt(pScore.firstChild.innerText);
-    if(score === 21){
-      this.pdateBalance(2);
-      dResManip.appElem("BLACKJACK!!");
-      dRes.style.color = "green";
-      nButton.style.display = "block";
-      bButton.style.display = "none";
-      hButton.style.display = "none";
-      sButton.style.display = "none";
-    }else if(avBalance.innerHTML == 0 && score > 21){
-      dResManip.appElem("The House Wins! Play Again?");
-      dRes.style.color = "red";
-      nButton.style.display = "none";
-      bButton.style.display = "none";
-      hButton.style.display = "none";
-      sButton.style.display = "none";
-      stButton.style.display = "block";
-    }else if(score > 21 ){
-      this.updateBalance(0);
-      dResManip.appElem("You've lost!");
-      dRes.style.color = "red";
-      nButton.style.display = "block";
-      bButton.style.display = "none";
-      hButton.style.display = "none";
-      sButton.style.display = "none";
-    }
-  }
-    
-}
+class App extends Component {
 
-class Cards{
-
-  constructor(table,score){
-    this.value = 0;
-    this.wTable = table;
-    this.wScore = score;
-    this.wScoreManip = new ElementManipulation(this.wScore);
-    this.wTableManip = new ElementManipulation(this.wTable);
-  }
-
-  checkCard = () => {
-    let cards = 0;
-    if (this.value === 10){
-      cards = highCards[Math.floor(Math.random()*4)];
-    }else if(this.value === 1){
-      cards = "A";
-    }else{
-    cards = this.value
-    }
-    return cards;
-  }
-
-  drawCard = () => {
-    this.genCard();
-    let newScore = parseInt(this.wScore.firstChild.innerText)+ parseInt(this.value);
-    console.log(newScore);
-    this.wScoreManip.repElem(newScore);
-    let aCard = this.checkCard();
-    let selectedColor = cardColor[Math.floor(Math.random()*4)];
-  
-    let cardImages = document.createElement("img");
-    cardImages.src=`./cards/${aCard}${selectedColor}.png`;
-  
-    this.wTable.appendChild(cardImages);
-  }
-
-  dispStartCards = () => {
-    let cards = 0;
-    let sum = 0;
-    for( let i =0; i< 2; i++){
-      let cardImages = document.createElement("img");
-      if(this.wTable !== dTable || i===1){
-        this.genCard();
-        sum+=this.value;
-        cards = this.checkCard();
-        let selectedColor = cardColor[Math.floor(Math.random()*4)];
-        cardImages.src=`./cards/${cards}${selectedColor}.png`;
-      }else{
-        cardImages.src=`./cards/blue_back.png`
-      }
-      this.wTable.appendChild(cardImages);
-    }
-    
-    this.wScoreManip.repElem(sum);
-  }
-
-  genCard = () =>{
-    this.value = Math.ceil(Math.random()*10);
-  }
-
-  clearCardsTable = () => {
-    this.wTableManip.delElem();
-    this.wScoreManip.repElem("0")
-    dResManip.delElem();
-    betArea.value="";
-    betArea.removeAttribute("disabled");
-    bButton.style.display = "block";
-    hButton.style.display = "none";
-    sButton.style.display = "none";
-  }
-
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  cardColor = ["C","D","H","S"];
-  highCards = ["10","J","K","Q"];
+  componentDidMount() {
   dScore = document.getElementById("dealerScore");
   pScore = document.getElementById("playerScore");
   nButton = document.getElementById("nButton");
@@ -232,17 +37,18 @@ document.addEventListener('DOMContentLoaded', function() {
   pTable = document.getElementById("playerTable");
   dTable = document.getElementById("dealerTable");
 
-  dResManip = new ElementManipulation(dRes);
-  pCards = new Cards(pTable,pScore);
-  dCards = new Cards(dTable,dScore);
-}, false);
+  pCards = new Cards(pTable,pScore,dRes);
+  dCards = new Cards(dTable,dScore,dRes);
 
+  console.log(bButton);
 
-
-
-let blackJack = new Game();
-
-class App extends Component {
+  blackJack = new Game(avBalance, betArea, pScore, hButton, sButton, nButton, stButton, bButton, dTable, dScore, dCards, dRes, pCards);
+  bButton.onclick = () => { blackJack.startGame() };
+  hButton.onclick = () => { pCards.drawCard(); blackJack.checkScore() };
+  sButton.onclick = () => { blackJack.dealerPlay() }
+  nButton.onclick = () => { blackJack.nextRound() };
+  stButton.onclick = () => { blackJack.newGame() };
+}
 
   shouldComponentUpdate() {
     return false;
@@ -278,15 +84,15 @@ class App extends Component {
                 <p>Available Balance: <span id="avBalance">1000</span></p>
               </Col>
               <Col>
-                <FormControl onChange={blackJack.checkInt.bind(this)} id="betValue" placeholder="Enter the amount you're willing to bet!"></FormControl>
+                <FormControl onChange={checkInt.bind(this)} id="betValue" placeholder="Enter the amount you're willing to bet!"></FormControl>
               </Col>
               <Col>
               <ButtonToolbar>
-                <Button id="bButton" onClick={blackJack.startGame} style={{"marginRight": "10px"}}>Bet</Button>
-                <Button id="hButton" onClick={() => {pCards.drawCard(); blackJack.checkScore();}} style={{"marginRight": "10px"}} variant="success">Hit me!</Button>
-                <Button id="sButton" onClick={ blackJack.dealerPlay } style={{"marginRight": "10px"}} variant="info">Stand</Button>
-                <Button id="nButton" onClick = { blackJack.nextRound } variant="warning">Next Round!</Button>
-                <Button id="stButton" onClick = { blackJack.newGame } variant="warning">Start New Game!</Button>
+                <Button id="bButton" style={{"marginRight": "10px"}}>Bet</Button>
+                <Button id="hButton" style={{"marginRight": "10px"}} variant="success">Hit me!</Button>
+                <Button id="sButton" style={{"marginRight": "10px"}} variant="info">Stand</Button>
+                <Button id="nButton" variant="warning">Next Round!</Button>
+                <Button id="stButton" variant="warning">Start New Game!</Button>
               </ButtonToolbar>
               </Col>
             </Row>
---
+++ src/classes.js
@@ -0,0 +1,224 @@
+
+
+export class ElementManipulation{
+
+    constructor(el){
+      this.elem = el;
+    }
+      delElem = () => {
+      while (this.elem.firstChild) {
+        this.elem.removeChild(this.elem.firstChild);
+      }
+    }
+    appElem = (txt) => {
+      let appendElem = document.createElement("p");
+      appendElem.innerText = txt;
+      this.elem.appendChild(appendElem);
+    }
+    repElem = (txt) => {
+      let newElem = document.createElement("p");
+      newElem.innerText = txt;
+      this.elem.replaceChild(newElem,this.elem.firstChild);
+    }
+  }
+  
+  export  class Game{
+
+    constructor(balance, bet, playerScore, hit, stand, next, start, betB, dealerTable, dealerScore, dealerCards, dispRes, playerCards){
+        this.avBalance = balance;
+        this.betArea = bet;
+        this.pScore = playerScore;
+        this.pCards = playerCards;
+        this.hButton = hit;
+        this.sButton = stand;
+        this.nButton = next;
+        this.stButton = start;
+        this.bButton = betB;
+        this.dTable = dealerTable;
+        this.dScore = dealerScore;
+        this.dCards = dealerCards;
+        this.dRes = dispRes;
+        this.dResManip = new ElementManipulation(this.dRes);
+    }
+  
+    updateBalance = (num) => {
+      console.log(this.avBalance.innerHTML)
+      if( num === 1)
+        this.avBalance.innerText = parseInt(this.avBalance.innerText) + parseInt(this.betArea.value)*2;
+      else if ( num === 2)
+        this.avBalance.innerText = parseInt(this.avBalance.innerText) + Math.ceil(parseInt(this.betArea.value)*(5/2));
+      else if ( num === 3)
+        this.avBalance.innerText = parseInt(this.avBalance.innerText) + parseInt(this.betArea.value);
+    }
+
+  
+    dealerPlay = () => {
+      let player = parseInt(this.pScore.firstChild.innerText)
+      this.hButton.style.display = "none";
+      this.sButton.style.display = "none";
+      this.dTable.removeChild(this.dTable.firstChild);
+      while (parseInt(this.dScore.firstChild.innerText) < 15)
+         this.dCards.drawCard();
+      if(this.avBalance.innerHTML == 0 && (parseInt(this.dScore.firstChild.innerText) <= 21 && parseInt(this.dScore.firstChild.innerText) > player)){
+        this.dResManip.appElem("The House Wins! Play Again?");
+        this.dRes.style.color = "red";
+        this.stButton.style.display = "block";
+        this.nButton.style.display = "none";
+      }else if(parseInt(this.dScore.firstChild.innerText) <= 21 && parseInt(this.dScore.firstChild.innerText) > player ){
+        this.dResManip.appElem("The dealer wins this round!");
+        this.dRes.style.color = "red";
+        this.nButton.style.display = "block";
+      }else if (parseInt(this.dScore.firstChild.innerText)=== player){
+        this.dResManip.appElem("Draw!")
+        this.dRes.style.color = "blue";
+        this.updateBalance(3);
+        this.nButton.style.display = "block";
+      }else{
+        this.dResManip.appElem("Congrats, you win this round!");
+        this.dRes.style.color = "green";
+        this.updateBalance(1);
+        this.nButton.style.display = "block";
+      }
+    }
+    
+    nextRound = () => {
+      this.pCards.clearCardsTable();
+      this.dCards.clearCardsTable();
+      this.nButton.style.display = "none";
+    }
+    
+    newGame = () => {
+      this.nextRound();
+      this.avBalance.innerHTML = 1000;
+      this.stButton.style.display = "none";
+    }
+    
+    startGame = () => {
+      let betValue = this.betArea.value;
+      console.log(betValue.length)
+    
+      if( !betValue.length ){
+        alert("Don't forget to enter a bet!")
+      }else if (betValue <= parseInt(this.avBalance.innerHTML)){
+        this.betArea.setAttribute("disabled","");
+        this.avBalance.innerHTML = parseInt(this.avBalance.innerHTML) - parseInt(betValue);
+        this.pCards.dispStartCards();
+        this.dCards.dispStartCards();
+        this.bButton.style.display = "none";
+        this.hButton.style.display = "block";
+        this.sButton.style.display = "block";
+      }else if(this.avBalance.innerHTML === 0){
+        this.stButton.style.display = "block";
+      }else{
+        alert("Cannot bet more than you have!")
+      }
+    }
+    
+    checkScore = () => {
+      let score = parseInt(this.pScore.firstChild.innerText);
+      if(score === 21){
+        this.pdateBalance(2);
+        this.dResManip.appElem("BLACKJACK!!");
+        this.dRes.style.color = "green";
+        this.nButton.style.display = "block";
+        this.bButton.style.display = "none";
+        this.hButton.style.display = "none";
+        this.sButton.style.display = "none";
+      }else if(this.avBalance.innerHTML == 0 && score > 21){
+        this.dResManip.appElem("The House Wins! Play Again?");
+        this.dRes.style.color = "red";
+        this.nButton.style.display = "none";
+        this.bButton.style.display = "none";
+        this.hButton.style.display = "none";
+        this.sButton.style.display = "none";
+        this.stButton.style.display = "block";
+      }else if(score > 21 ){
+        this.updateBalance(0);
+        this.dResManip.appElem("You've lost!");
+        this.dRes.style.color = "red";
+        this.nButton.style.display = "block";
+        this.bButton.style.display = "none";
+        this.hButton.style.display = "none";
+        this.sButton.style.display = "none";
+      }
+    }
+      
+  }
+  
+  export  class Cards{
+  
+    constructor(table,score, res){
+      this.value = 0;
+      this.wTable = table;
+      this.wScore = score;
+      this.wScoreManip = new ElementManipulation(this.wScore);
+      this.wTableManip = new ElementManipulation(this.wTable);
+      this.dRes = res;
+      this.dResManip = new ElementManipulation(this.dRes);
+      this.cardColor = ["C","D","H","S"];
+      this.highCards = ["10","J","K","Q"];
+    }
+  
+    checkCard = () => {
+      let cards = 0;
+      if (this.value === 10){
+        cards = this.highCards[Math.floor(Math.random()*4)];
+      }else if(this.value === 1){
+        cards = "A";
+      }else{
+      cards = this.value
+      }
+      return cards;
+    }
+  
+    drawCard = () => {
+      this.genCard();
+      let newScore = parseInt(this.wScore.firstChild.innerText)+ parseInt(this.value);
+      console.log(newScore);
+      this.wScoreManip.repElem(newScore);
+      let aCard = this.checkCard();
+      let selectedColor = this.cardColor[Math.floor(Math.random()*4)];
+    
+      let cardImages = document.createElement("img");
+      cardImages.src=`./cards/${aCard}${selectedColor}.png`;
+    
+      this.wTable.appendChild(cardImages);
+    }
+  
+    dispStartCards = () => {
+      let cards = 0;
+      let sum = 0;
+      for( let i =0; i< 2; i++){
+        let cardImages = document.createElement("img");
+        if(this.wTable !== this.dTable || i===1){
+          this.genCard();
+          sum+=this.value;
+          cards = this.checkCard();
+          let selectedColor = this.cardColor[Math.floor(Math.random()*4)];
+          cardImages.src=`./cards/${cards}${selectedColor}.png`;
+        }else{
+          cardImages.src=`./cards/blue_back.png`
+        }
+        this.wTable.appendChild(cardImages);
+      }
+      
+      this.wScoreManip.repElem(sum);
+    }
+  
+    genCard = () =>{
+      this.value = Math.ceil(Math.random()*10);
+    }
+  
+    clearCardsTable = () => {
+      this.wTableManip.delElem();
+      this.wScoreManip.repElem("0")
+      this.dResManip.delElem();
+      this.betArea.value="";
+      this.betArea.removeAttribute("disabled");
+      this.bButton.style.display = "block";
+      this.hButton.style.display = "none";
+      this.sButton.style.display = "none";
+    }
+  
+  }
+  
